6acc462e14518eda3083227b095070df95c8d940
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-12-16 11:57:42 +0000

    Updates !

--- app/ModuleServiceProvider.php
+++ app/ModuleServiceProvider.php
@@ -3,10 +3,19 @@
 namespace BristolSU\Module\Typeform;
 
 use BristolSU\Module\Typeform\Commands\CreateWebhooks;
+use BristolSU\Module\Typeform\Connectors\Typeform as TypeformApiKeyConnector;
 use BristolSU\Module\Typeform\Support\Webhooks\Contracts\WebhookLinker as WebhookLinkerContract;
 use BristolSU\Module\Typeform\Support\Webhooks\WebhookLinker;
+use BristolSU\Support\Connection\Contracts\ConnectorStore;
+use BristolSU\Support\Connection\Contracts\ServiceRequest;
 use BristolSU\Support\Module\ModuleServiceProvider as ServiceProvider;
+use FormSchema\Generator\Field;
+use FormSchema\Generator\Form as FormGenerator;
+use FormSchema\Generator\Group;
+use FormSchema\Schema\Form;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use WATR\Typeform;
 
 class ModuleServiceProvider extends ServiceProvider
@@ -61,6 +70,54 @@ class ModuleServiceProvider extends ServiceProvider
         Route::bind($this->alias() . '_file', function($id) {
             return File::findOrFail($id);
         });
+        
+        /** @var ConnectorStore $connectorStore */
+        $connectorStore = app(ConnectorStore::class);
+        $connectorStore->register(
+            'Typeform API Key', 'Connect to Typeform using an API Key',
+            'typeform_api_key', 'typeform', 
+            TypeformApiKeyConnector::class);
+        
+        $this->registerGlobalScript('modules/typeform/js/components.js');
+        
+        app(ServiceRequest::class)->required($this->alias(), ['typeform']);
+        
+    }
+
+    public function settings(): Form
+    {
+        return FormGenerator::make()->withGroup(
+            Group::make('Page Design')->withField(
+                Field::input('title')->inputType('text')->label('Module Title')->default('Page Title')
+            )->withField(
+                Field::textArea('description')->label('Description')->hint('This will appear at the top of the page')->rows(4)->default('Description')
+            )
+        )->withGroup(
+            Group::make('Embedded Form')->withField(
+                Field::radios('embed_type')->inputType('text')->label('Type of form embedding')->hint('Embed the form in the page, or show the form as a popup?')
+                    ->values([
+                        ['name' => 'Embed the form in the page', 'value' => 'widget'],
+                        ['name' => 'Show the form as a popup', 'value' => 'popup'],
+                        ['name' => 'Show the form as a drawer from the left', 'value' => 'drawer_left'],
+                        ['name' => 'Show the form as a drawer from the right', 'value' => 'drawer_right'],
+                    ])->default('widget')
+            )->withField(
+                Field::input('form_url')->inputType('text')->label('Form URL')->hint('The URL of the form. Make sure it\'s published first!')
+            )->withField(
+                Field::switch('hide_headers')->label('Hide form headers?')->hint('Should we hide the form headers? This helps integrate the form into the page.')
+                    ->textOn('Hidden')->textOff('Shown')->default(true)
+            )->withField(
+                Field::switch('hide_footer')->label('Hide form footer?')->hint('Should we hide the form footer? This helps integrate the form into the page.')
+                    ->textOn('Hidden')->textOff('Shown')->default(true)
+            )
+        )->withGroup(
+            Group::make('Responses')->withField(
+                Field::switch('collect_responses')->label('Save responses?')->hint('Do you want responses to be saved on the portal? You will always be able to see responses on typeform.')
+                    ->textOn('Save')->textOff('Do not save')->default(true)
+            )->withField(
+                Field::input('form_id')->inputType('text')->label('Form ID')->hint('ID of the form so we can collect responses')
+            )
+        )->getSchema();
     }
     
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ const mix = require('laravel-mix');
 mix.setPublicPath('./public');
 
 mix.js('resources/js/module.js', 'public/modules/typeform/js')
+    .js('resources/js/components.js', 'public/modules/typeform/js')
     .sass('resources/sass/module.scss', 'public/modules/typeform/css');
